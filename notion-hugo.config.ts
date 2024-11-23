bc8b728f0c7aee0a0016f648190728f9073e75bf
Hongguang Wang <truman@pku.edu.cn>
2024-11-23 22:46:28 +0800

    Update notion-hugo.config.ts

--- notion-hugo.config.ts
+++ notion-hugo.config.ts
@@ -3,7 +3,7 @@ import { UserConfig } from "./src/config"
 const userConfig: UserConfig = {
     mount: {
         manual: false,
-        page_url: 'https://pcloud.notion.site/Notion-DoIt-04bcc51cfe4c49938229c35e4f0a6fb6',
+        page_url: 'https://www.notion.so/Notion-DoIt-147e95eba1bf80ef8eb6d8bf33bbec71?pvs=4',
         pages: [
             // {
             //     page_id: '<page_id>',
